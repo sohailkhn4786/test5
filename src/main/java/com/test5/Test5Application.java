d73fc09d1af298d1701456b05b9247862de0d9af
sohailkhn4786 <sohailkhn4786@gmail.com>
2025-03-24 23:37:32 +0530

    second commit

--- src/main/java/com/test5/Test5Application.java
+++ src/main/java/com/test5/Test5Application.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test5Application {
 
 	public static void main(String[] args) {
-              int x=100;
+            String name="mike";
+             int x=100;
 		SpringApplication.run(Test5Application.class, args);
 	}
 
